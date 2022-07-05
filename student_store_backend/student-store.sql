ff55ba043477c4038603bb3aa7813bf7e5f2581b
WinsonChen114 <14winsonchen@gmail.com>
2022-07-05 14:17:58 -0400

    Added more sql tables

--- student_store_backend/student-store.sql
+++ student_store_backend/student-store.sql
@@ -6,6 +6,7 @@ CREATE DATABASE student_store;
 \connect student_store
 
 \i student-store-schema.sql
+\i student-store-seed.sql
 
 \echo 'Delete and recreate student_store_test db?'
 \prompt 'Return for yes or control-C to cancel > ' answer
@@ -15,3 +16,4 @@ CREATE DATABASE student_store_test;
 \connect student_store_test
 
 \i student-store-schema.sql
+
